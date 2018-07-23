e14c30a30afe6cdaafb344f505b1398d15555eea
brendadev <brenda@koneksahealth.com>
2018-07-23 19:05:54 -0400

    cleanup

--- app/src/main/java/com/example/android/princeton/ItemText.java
+++ app/src/main/java/com/example/android/princeton/ItemText.java
@@ -4,22 +4,14 @@ public class ItemText {
     private String mName;
     private String mTown;
 
-    /** image resource is not provided **/
-    private static final int NO_IMAGE_PROVIDED = -1;
-    /** image resource id **/
-    private int mImageResourceId = NO_IMAGE_PROVIDED;
-//
+    private int mImageResourceId;
+
     public  ItemText(String name, String town, int imageResourceId) {
         mName = name;
         mTown = town;
         mImageResourceId = imageResourceId;
     }
-
-//    public ItemText(String name, String town) {
-//        mName = name;
-//        mTown = town;
-//    }
-
+    
     public String getName() {
         return mName;
     }
@@ -30,9 +22,5 @@ public class ItemText {
 
     public int getImageResourceId() {
         return mImageResourceId;
-    }
-
-    public boolean hasValidImage() {
-        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
